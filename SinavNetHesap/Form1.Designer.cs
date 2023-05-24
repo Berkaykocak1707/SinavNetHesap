831b3c371165243399d5d30e5eaaaaecced876f3
Berkay Kocak <88945838+Berkaykocak1707@users.noreply.github.com>
2023-05-24 13:22:16 +0300

    Add project files.

---
+++ SinavNetHesap/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace SinavNetHesap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TurkceDogrutextBox = new System.Windows.Forms.TextBox();
+            this.MatDogrutextBox = new System.Windows.Forms.TextBox();
+            this.FenDogrutextBox = new System.Windows.Forms.TextBox();
+            this.SosyalDogrutextBox = new System.Windows.Forms.TextBox();
+            this.TurkceYanlistextBox = new System.Windows.Forms.TextBox();
+            this.MatYanlistextBox = new System.Windows.Forms.TextBox();
+            this.FenYanlistextBox = new System.Windows.Forms.TextBox();
+            this.SosyalYanlistextBox = new System.Windows.Forms.TextBox();
+            this.TurkceNettextBox = new System.Windows.Forms.TextBox();
+            this.MatNettextBox = new System.Windows.Forms.TextBox();
+            this.FenNettextBox = new System.Windows.Forms.TextBox();
+            this.SosyalNettextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Türkçe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Matematik";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Fen";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(52, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Sosyal";
+            // 
+            // TurkceDogrutextBox
+            // 
+            this.TurkceDogrutextBox.Location = new System.Drawing.Point(114, 63);
+            this.TurkceDogrutextBox.Name = "TurkceDogrutextBox";
+            this.TurkceDogrutextBox.Size = new System.Drawing.Size(194, 20);
+            this.TurkceDogrutextBox.TabIndex = 1;
+            // 
+            // MatDogrutextBox
+            // 
+            this.MatDogrutextBox.Location = new System.Drawing.Point(114, 102);
+            this.MatDogrutextBox.Name = "MatDogrutextBox";
+            this.MatDogrutextBox.Size = new System.Drawing.Size(194, 20);
+            this.MatDogrutextBox.TabIndex = 1;
+            // 
+            // FenDogrutextBox
+            // 
+            this.FenDogrutextBox.Location = new System.Drawing.Point(114, 142);
+            this.FenDogrutextBox.Name = "FenDogrutextBox";
+            this.FenDogrutextBox.Size = new System.Drawing.Size(194, 20);
+            this.FenDogrutextBox.TabIndex = 1;
+            // 
+            // SosyalDogrutextBox
+            // 
+            this.SosyalDogrutextBox.Location = new System.Drawing.Point(114, 187);
+            this.SosyalDogrutextBox.Name = "SosyalDogrutextBox";
+            this.SosyalDogrutextBox.Size = new System.Drawing.Size(194, 20);
+            this.SosyalDogrutextBox.TabIndex = 1;
+            // 
+            // TurkceYanlistextBox
+            // 
+            this.TurkceYanlistextBox.Location = new System.Drawing.Point(361, 63);
+            this.TurkceYanlistextBox.Name = "TurkceYanlistextBox";
+            this.TurkceYanlistextBox.Size = new System.Drawing.Size(194, 20);
+            this.TurkceYanlistextBox.TabIndex = 1;
+            // 
+            // MatYanlistextBox
+            // 
+            this.MatYanlistextBox.Location = new System.Drawing.Point(361, 102);
+            this.MatYanlistextBox.Name = "MatYanlistextBox";
+            this.MatYanlistextBox.Size = new System.Drawing.Size(194, 20);
+            this.MatYanlistextBox.TabIndex = 1;
+            // 
+            // FenYanlistextBox
+            // 
+            this.FenYanlistextBox.Location = new System.Drawing.Point(361, 142);
+            this.FenYanlistextBox.Name = "FenYanlistextBox";
+            this.FenYanlistextBox.Size = new System.Drawing.Size(194, 20);
+            this.FenYanlistextBox.TabIndex = 1;
+            // 
+            // SosyalYanlistextBox
+            // 
+            this.SosyalYanlistextBox.Location = new System.Drawing.Point(361, 187);
+            this.SosyalYanlistextBox.Name = "SosyalYanlistextBox";
+            this.SosyalYanlistextBox.Size = new System.Drawing.Size(194, 20);
+            this.SosyalYanlistextBox.TabIndex = 1;
+            // 
+            // TurkceNettextBox
+            // 
+            this.TurkceNettextBox.Enabled = false;
+            this.TurkceNettextBox.Location = new System.Drawing.Point(600, 63);
+            this.TurkceNettextBox.Name = "TurkceNettextBox";
+            this.TurkceNettextBox.Size = new System.Drawing.Size(194, 20);
+            this.TurkceNettextBox.TabIndex = 1;
+            // 
+            // MatNettextBox
+            // 
+            this.MatNettextBox.Enabled = false;
+            this.MatNettextBox.Location = new System.Drawing.Point(600, 102);
+            this.MatNettextBox.Name = "MatNettextBox";
+            this.MatNettextBox.Size = new System.Drawing.Size(194, 20);
+            this.MatNettextBox.TabIndex = 1;
+            // 
+            // FenNettextBox
+            // 
+            this.FenNettextBox.Enabled = false;
+            this.FenNettextBox.Location = new System.Drawing.Point(600, 142);
+            this.FenNettextBox.Name = "FenNettextBox";
+            this.FenNettextBox.Size = new System.Drawing.Size(194, 20);
+            this.FenNettextBox.TabIndex = 1;
+            // 
+            // SosyalNettextBox
+            // 
+            this.SosyalNettextBox.Enabled = false;
+            this.SosyalNettextBox.Location = new System.Drawing.Point(600, 187);
+            this.SosyalNettextBox.Name = "SosyalNettextBox";
+            this.SosyalNettextBox.Size = new System.Drawing.Size(194, 20);
+            this.SosyalNettextBox.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(187, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Doğru";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(434, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Yanlış";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(671, 35);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Net";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(719, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(52, 257);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(572, 14);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Matematik 40, Türkçe 40, Sosyal 20, Fen 20 sorudur. Her yanlış 0,25 net puan, 4 y" +
+    "anlış 1 doğruyu götürür.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 311);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SosyalNettextBox);
+            this.Controls.Add(this.FenNettextBox);
+            this.Controls.Add(this.SosyalYanlistextBox);
+            this.Controls.Add(this.FenYanlistextBox);
+            this.Controls.Add(this.MatNettextBox);
+            this.Controls.Add(this.SosyalDogrutextBox);
+            this.Controls.Add(this.MatYanlistextBox);
+            this.Controls.Add(this.TurkceNettextBox);
+            this.Controls.Add(this.FenDogrutextBox);
+            this.Controls.Add(this.TurkceYanlistextBox);
+            this.Controls.Add(this.MatDogrutextBox);
+            this.Controls.Add(this.TurkceDogrutextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TurkceDogrutextBox;
+        private System.Windows.Forms.TextBox MatDogrutextBox;
+        private System.Windows.Forms.TextBox FenDogrutextBox;
+        private System.Windows.Forms.TextBox SosyalDogrutextBox;
+        private System.Windows.Forms.TextBox TurkceYanlistextBox;
+        private System.Windows.Forms.TextBox MatYanlistextBox;
+        private System.Windows.Forms.TextBox FenYanlistextBox;
+        private System.Windows.Forms.TextBox SosyalYanlistextBox;
+        private System.Windows.Forms.TextBox TurkceNettextBox;
+        private System.Windows.Forms.TextBox MatNettextBox;
+        private System.Windows.Forms.TextBox FenNettextBox;
+        private System.Windows.Forms.TextBox SosyalNettextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ SinavNetHesap/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SinavNetHesap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double turkcedogru = 0, turkceyanlis = 0, matdogru = 0, matyanlis = 0, fendogru = 0, fenyanlis = 0, sosyaldogru = 0, sosyalyanlis = 0, turkcenet = 0, matnet = 0, fennet = 0, sosyalnet = 0, matsorusayi=0,turkcesorusayi=0,fensorusayi=0,soysalsorusayi=0;
+            
+            if (TurkceDogrutextBox.Text == "" || TurkceYanlistextBox.Text =="" || MatDogrutextBox.Text=="" || MatYanlistextBox.Text==""|| FenDogrutextBox.Text == "" || FenYanlistextBox.Text == "" || SosyalDogrutextBox.Text==""|| SosyalYanlistextBox.Text=="")
+            {
+                MessageBox.Show("Lütfen bütün kutucukları doldurunuz.");
+            }
+            else
+            {
+                turkcedogru = Convert.ToDouble(TurkceDogrutextBox.Text);
+                turkceyanlis = Convert.ToDouble(TurkceYanlistextBox.Text);
+
+                matdogru = Convert.ToDouble(MatDogrutextBox.Text);
+                matyanlis = Convert.ToDouble(MatYanlistextBox.Text);
+
+                fendogru = Convert.ToDouble(FenDogrutextBox.Text);
+                fenyanlis = Convert.ToDouble(FenYanlistextBox.Text);
+
+                sosyaldogru = Convert.ToDouble(SosyalDogrutextBox.Text);
+                sosyalyanlis = Convert.ToDouble(SosyalYanlistextBox.Text);
+
+                turkcenet = turkcedogru - (turkceyanlis * 0.25);
+                matnet = matdogru - (matyanlis * 0.25);
+                fennet = fendogru - (fenyanlis * 0.25);
+                sosyalnet = sosyaldogru - (sosyalyanlis * 0.25);
+
+                matsorusayi = Convert.ToInt32(MatDogrutextBox.Text) + Convert.ToInt32(MatYanlistextBox.Text);
+                turkcesorusayi = Convert.ToInt32(TurkceDogrutextBox.Text) + Convert.ToInt32(TurkceYanlistextBox.Text);
+                fensorusayi = Convert.ToInt32(FenDogrutextBox.Text) + Convert.ToInt32(FenYanlistextBox.Text);
+                soysalsorusayi = Convert.ToInt32(SosyalDogrutextBox.Text) + Convert.ToInt32(SosyalYanlistextBox.Text);
+            }
+            if (matsorusayi > 40 || turkcesorusayi > 40)
+            {
+                MessageBox.Show("Türkçe veya Matematikte yanlış değer girildi, her iki ders de 40 sorudur. Doğru ve Yanlış sayısını kontol ediniz.");
+            }
+            else if (fensorusayi > 20 || soysalsorusayi > 20)
+            {
+                MessageBox.Show("Fen veya Sosyalde yanlış değer girildi, her iki ders de 20 sorudur. Doğru ve Yanlış sayısını kontol ediniz.");
+            }
+            else
+            {
+                TurkceNettextBox.Text = turkcenet.ToString();
+                MatNettextBox.Text = matnet.ToString();
+                FenNettextBox.Text = fennet.ToString();
+                SosyalNettextBox.Text = sosyalnet.ToString();
+                if (turkcenet < 20)
+                {
+                    TurkceNettextBox.BackColor = Color.Red;
+                    TurkceNettextBox.ForeColor = Color.White;
+                }
+                else
+                {
+                    TurkceNettextBox.BackColor = Color.White;
+                }
+                if (matnet < 20)
+                {
+                    MatNettextBox.BackColor = Color.Red;
+                    MatNettextBox.ForeColor = Color.White;
+                }
+                else
+                {
+                    MatNettextBox.BackColor = Color.White;
+                }
+                if (fennet < 10)
+                {
+                    FenNettextBox.BackColor = Color.Red;
+                    FenNettextBox.ForeColor = Color.White;
+                }
+                else
+                {
+                    FenNettextBox.BackColor = Color.White;
+                }
+                if (sosyalnet < 10)
+                {
+                    SosyalNettextBox.BackColor = Color.Red;
+                    SosyalNettextBox.ForeColor = Color.White;
+                }
+                else
+                {
+                    SosyalNettextBox.BackColor = Color.White;
+                }
+            }
+
+        }
+    }
+}
